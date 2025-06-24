560b7f6c601aa2ee6bbbc3e12f299f29c155d674
Utkarsh Singh <singhutkarshpbh@gmail.com>
2025-06-25 02:25:55 +0530

    Voice agent to be added

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import { useParams } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 function MedicalVoiceAgent() {
+
+  const {sessionId} = useParams();
+
+  useEffect(()=>{
+    sessionId&&GetSessionDetails();
+  },[sessionId])
+  const GetSessionDetails = async()=> {
+    const result = await axios.get('/api/session-chat?sessionId='+sessionId)
+    console.log(result.data);
+  }
   return (
     <div>
       MedicalVoiceAgent
