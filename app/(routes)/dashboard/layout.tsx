f52ea8a2e7081989c18c8560705dba34c26bfc63
singhutkarshpbh <146609958+singhutkarshpbh@users.noreply.github.com>
2025-06-24 17:39:49 +0000

    new changes

---
+++ app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>{children}</div>
+  )
+}
+
+export default DashboardLayout
