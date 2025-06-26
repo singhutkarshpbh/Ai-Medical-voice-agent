f046cbbc3b14cc293efc0fc81841b41229fb95a2
Utkarsh Singh <singhutkarshpbh@gmail.com>
2025-06-26 17:24:51 +0530

    changed logo

--- app/(routes)/dashboard/_components/AppHeader.tsx
+++ app/(routes)/dashboard/_components/AppHeader.tsx
@@ -29,7 +29,7 @@ const menuOptions = [
 function AppHeader() {
   return (
     <div className='flex items-center justify-between p-4 shadow px-10 md:px-20 lg:px-40'>
-        <Image src= {'/logo.svg'} alt = 'logo' width ={120} height ={90} />
+        <Image src= {'/logo.png'} alt = 'logo' width ={120} height ={90} />
         <div className='hidden md:flex gap-12 items-center'>
             {menuOptions.map((option,index)=> (
                 <Link key= {index} href={option.path}> 
